3cd863172022219f99108f835e3de4c6d84f35d8
LeiZ54 <zlei9549@gmail.com>
2025-02-25 19:58:32 -0500

    first commit

---
+++ server/src/main/java/org/lei/biil_buddy/BiilBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.lei.biil_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiilBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiilBuddyApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/org/lei/biil_buddy/BiilBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lei.biil_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiilBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
