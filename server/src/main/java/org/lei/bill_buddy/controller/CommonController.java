c051f80ba7970a4c2f987c7de108603127bea1e1
LeiZ54 <zlei9549@gmail.com>
2025-04-26 17:26:08 -0400

    Corrected the return order of elements in enumerated classes

--- server/src/main/java/org/lei/bill_buddy/controller/CommonController.java
+++ server/src/main/java/org/lei/bill_buddy/controller/CommonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -21,18 +22,48 @@ public class CommonController {
     @GetMapping("/group-types")
     public Map<String, String> getGroupTypes() {
         return Arrays.stream(GroupType.values())
-                .collect(Collectors.toMap(Enum::name, GroupType::getImageUrl));
+                .sorted((g1, g2) -> {
+                    if (g1 == GroupType.OTHER) return -1;
+                    if (g2 == GroupType.OTHER) return 1;
+                    return g1.name().compareTo(g2.name());
+                })
+                .collect(Collectors.toMap(
+                        Enum::name,
+                        GroupType::getImageUrl,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
     }
 
     @GetMapping("/expense-types")
     public Map<String, String> getExpenseTypes() {
         return Arrays.stream(ExpenseType.values())
-                .collect(Collectors.toMap(Enum::name, ExpenseType::getImageUrl));
+                .sorted((e1, e2) -> {
+                    if (e1 == ExpenseType.OTHER) return -1;
+                    if (e2 == ExpenseType.OTHER) return 1;
+                    return e1.name().compareTo(e2.name());
+                })
+                .collect(Collectors.toMap(
+                        Enum::name,
+                        ExpenseType::getImageUrl,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
     }
 
     @GetMapping("/currencies")
     public Map<String, String> getCurrencies() {
         return Arrays.stream(Currency.values())
-                .collect(Collectors.toMap(Enum::name, Currency::getSymbol));
+                .sorted((c1, c2) -> {
+                    if (c1 == Currency.USD) return -1;
+                    if (c2 == Currency.USD) return 1;
+                    return c1.name().compareTo(c2.name());
+                })
+                .collect(Collectors.toMap(
+                        Enum::name,
+                        Currency::getSymbol,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
     }
 }
